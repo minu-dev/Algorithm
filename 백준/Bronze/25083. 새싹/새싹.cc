4c022cd8b8d94ff994a4e4c5d5a30ddc60379d3a
minu-dev <88576607+minu-dev@users.noreply.github.com>
2023-09-02 14:37:29 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,15 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 100
+
+int main() {
+  printf("         ,r\'\"7\n");
+  printf("r`-_   ,\'  ,/\n");
+  printf(" \\. \". L_r\'\n");
+  printf("   `~\\/\n");
+  printf("      |\n");
+  printf("      |\n");
+  return 0;
+}
