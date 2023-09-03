d2129dcff4ff2c3943bb58749c3c6446ddf9c2f1
minu-dev <88576607+minu-dev@users.noreply.github.com>
2023-09-03 13:19:43 +0900

    Update update.py

--- update.py
+++ update.py
@@ -3,22 +3,15 @@
 import os
 from urllib import parse
 
-HEADER = "```C\nprintf(\"Baekjoon\");\n```\n##\n"
-
 def main():
     readme = ""
-    readme += HEADER
     content = ""
     
     directories = [];
     solveds = [];
 
-    bronze = 0
-    silver = 0
-    gold = 0
-    platinum = 0
-    diamond = 0
-    ruby = 0
+    total = 0
+    tier = {"Bronze": 0, "Silver": 0, "Gold": 0, "Platinum": 0, "Diamond": 0, "Ruby": 0}
 
     for root, dirs, files in os.walk("."):
         dirs.sort()
@@ -39,18 +32,11 @@ def main():
         
         if directory == '.':
             continue
-        elif directory == "Bronze":
-            bronze += 1
-        elif directory == "Silver":
-            silver += 1
-        elif directory == "Gold":
-            gold += 1
-        elif directory == "Platinum":
-            platinum += 1
-        elif directory == "Diamond":
-            diamond += 1
-        elif directory == "Ruby":
-            ruby += 1
+        else:
+            try:
+                tier[directory] += 1
+            except:
+                pass
 
         for file in files:
             if category not in solveds:
@@ -59,9 +45,33 @@ def main():
                 content += "| {}. | {} | [\U0001F517]({}) |\n".format(number, name, parse.quote(os.path.join(root, file)))
                 solveds.append(category)
                 print("category : " + category)
-              
-    total = bronze + silver + gold + platinum + diamond + ruby
-    readme += "```C\nstatic int total = {};\n```\n##\n\n```C\nenum {{\n    BRONZE = {},\n    SILVER = {},\n    GOLD = {},\n    PLATINUM = {},\n    DIAMOND = {},\n    RUBY = {}\n}};\n```\n##\n```C\nprintf(\"Problem Lists\");\n```\n##\n| No. | Problem | Link |\n|:-:|:-|:-:|\n".format(total, bronze, silver, gold, platinum, diamond, ruby)
+
+    for value in tier.values():
+        total += value
+        
+    readme += ("```C\n" + 
+               "printf(\"Baekjoon\");\n" + 
+               "```\n" + 
+               "##\n" +
+               "```C\n" + 
+               "static int total = {};\n".format(total) + 
+               "```\n" +
+               "##\n\n" + 
+               "```C\n" + 
+               "enum {{\n" + 
+               "    BRONZE = {},\n".format(tier["Bronze"]) + 
+               "    SILVER = {},\n".format(tier["Silver"]) +
+               "    GOLD = {},\n".format(tier["Gold"]) + 
+               "    PLATINUM = {},\n".format(tier["Platinum"]) + 
+               "    DIAMOND = {},\n".format(tier["Diamond"]) + 
+               "    RUBY = {}\n}};\n".format(tier["Ruby"]) + 
+               "```\n" + 
+               "##\n" + 
+               "```C\n" + 
+               "printf(problems);\n" + 
+               "```\n" + 
+               "| No. | Problem | Link |\n" + 
+               "|:---:|:--------|:----:|\n")
     
     readme += content
     
