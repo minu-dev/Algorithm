3ec145860028374fc3bf9766ed9145efbad74d6b
minu-dev <88576607+minu-dev@users.noreply.github.com>
2024-10-01 16:28:46 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -3,19 +3,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define R 31
 #define M 1234567891
 
 int main() {
-  int L, hash = 0;
+  int L;
+  long long unsigned int R = 1;
+  long long unsigned int hash = 0;
   scanf("%d", &L);
   char *str = (char *)malloc(L * sizeof(char));
   scanf("%s", str);
-  for (int i = 0; i < strlen(str); i++)
-    hash += (str[i] - 96) * pow(R, i);
-  //알파벳 소문자는 아스키번호 97부터 시작이므로
-  // 96을 빼주어 a가 1, b가 2, c가 3 ... 이 되도록 함
+  for (int i = 0; i < strlen(str); i++) {
+    hash += (long long unsigned int)(str[i] - 96) * R;
+    //알파벳 소문자는 아스키번호 97부터 시작이므로
+    // 96을 빼주어 a가 1, b가 2, c가 3 ... 이 되도록 함
+    R *= 31;
+    R %= M;
+    // R을 거듭제곱 한 후, 너무 커지기 전에 mod M 연산 수행함
+  }
   hash %= M;
-  printf("%d\n", hash);
+  printf("%lld\n", hash);
   return 0;
 }
