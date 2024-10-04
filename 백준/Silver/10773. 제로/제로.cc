1959ec659b5300174f17b3da318d1c4f7506bc0d
minu-dev <88576607+minu-dev@users.noreply.github.com>
2024-10-04 22:36:13 +0900

    [Silver IV] Title: 제로, Time: 8 ms, Memory: 1500 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.cc
@@ -0,0 +1,59 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_STACK_SIZE 100000
+//문제 특성상 fgets로 입력 받아야 하는데,
+// fgets는 저장 시 개행 문자 + 널 문자를 뒤에 붙여서 저장함.
+//따라서 스택의 최대 크기를 102로 설정해 주어야함.
+
+int stack[MAX_STACK_SIZE] = {
+    0,
+};
+int top = -1;
+
+int IsEmpty() {
+  if (top < 0)
+    return 1;
+  else
+    return 0;
+}
+
+int IsFull() {
+  if (top >= MAX_STACK_SIZE - 1)
+    return 1;
+  else
+    return 0;
+}
+
+void push(int value) {
+  if (!IsFull())
+    stack[++top] = value;
+}
+
+int pop() {
+  if (!IsEmpty()) {
+    int tmp = stack[top];
+    stack[top--] = 0;
+    return tmp;
+  } else
+    return 0;
+}
+
+int main() {
+  int k;
+  int n;
+  int sum = 0;
+  scanf("%d", &k);
+  for (int i = 0; i < k; i++) {
+    scanf("%d", &n);
+    if (n == 0)
+      pop();
+    else
+      push(n);
+  }
+  while (!IsEmpty())
+    sum += pop();
+  printf("%d", sum);
+  return 0;
+}
