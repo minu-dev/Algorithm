7af74a73b3bdde7747773724f50e3a1764134b47
minu-dev <88576607+minu-dev@users.noreply.github.com>
2024-10-06 22:22:16 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int T;
+  int a, b;
+  scanf("%d", &T);
+  for (int i = 0; i < T; i++) {
+    scanf("%d %d", &a, &b);
+    printf("Case #%d: %d + %d = %d\n", i + 1, a, b, a + b);
+  }
+  return 0;
+}
