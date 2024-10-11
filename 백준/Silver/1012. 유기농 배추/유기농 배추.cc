bfb343539c6a6bab75fe382443c20e96de6afd8d
minu-dev <88576607+minu-dev@users.noreply.github.com>
2024-10-11 15:44:09 +0900

    Update 유기농 배추.cc

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -14,6 +14,10 @@ void init() {
 
 void DFS(int x, int y) {
   arr[x][y] = 0;
+  // else if 사용 시 한 칸 만 붙어있는게 아닌 경우
+  // 예를 들어, 왼쪽에도 1이 있고, 오른쪽에도 1이 있는 경우
+  // 이러한 케이스를 포함하지 못함
+  // 따라서 모두 if로 사용
   if (arr[x + 1][y])
     DFS(x + 1, y);
   if (arr[x - 1][y])
