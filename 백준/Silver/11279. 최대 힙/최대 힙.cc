3e4247933fb4a8d933d067311bb89003b6e471f9
minu-dev <88576607+minu-dev@users.noreply.github.com>
2024-10-13 22:32:53 +0900

    [Silver II] Title: 최대 힙, Time: 20 ms, Memory: 1508 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,64 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void heapfify(int *heap, int size, int start) {
+  int max = start;
+  int left = 2 * start + 1;
+  int right = left + 1;
+
+  if (left <= size && heap[max] < heap[left])
+    max = left;
+  if (right <= size && heap[max] < heap[right])
+    max = right;
+
+  if (max != start) {
+    int tmp = heap[start];
+    heap[start] = heap[max];
+    heap[max] = tmp;
+    heapfify(heap, size, max);
+  }
+}
+
+void insert(int *heap, int index, int data) {
+  heap[index] = data;
+  if (index != 0) {
+    while (heap[index] > heap[(index - 1) / 2]) {
+      int tmp = heap[index];
+      heap[index] = heap[(index - 1) / 2];
+      heap[(index - 1) / 2] = tmp;
+      index = (index - 1) / 2;
+    }
+  }
+}
+
+int eliminate(int *heap, int size) {
+  int tmp = heap[0];
+  heap[0] = heap[size];
+  heap[size] = -1;
+  heapfify(heap, size, 0);
+  return tmp;
+}
+
+int main() {
+  int n;
+  int size = 0;
+  scanf("%d", &n);
+  int *heap = (int *)malloc(sizeof(int) * n);
+  for (int i = 0; i < n; i++) {
+    heap[i] = -1;
+  }
+  for (int i = 0; i < n; i++) {
+    int x;
+    scanf("%d", &x);
+    if (x == 0) {
+      if (size == 0)
+        printf("%d\n", 0);
+      else {
+        printf("%d\n", eliminate(heap, --size));
+      }
+    } else {
+      insert(heap, size++, x);
+    }
+  }
+}
